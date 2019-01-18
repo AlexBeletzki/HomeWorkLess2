5f0c663fb18d0157bca4d70637cb0da8c4ea65bd
Alex Beletzki <belecky87@gmail.com>
2019-01-18 16:27:38 +0300

    Решена вторая задача урока Easy

--- Home Work Lesson 2.py
+++ Home Work Lesson 2.py
@@ -16,3 +16,20 @@
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 for num, frut in enumerate(fruits):
     print (str(num) + '.  {:>8}'.format(frut))
+
+
+# Задача-2:
+# Даны два произвольные списка.
+# Удалите из первого списка элементы, присутствующие во втором списке.
+
+Marvel = ['Captain America', 'Superman', 'Thor', 'Spider-Man', 'Hawkeye', 'Batman', 'Iron Man', 'BlackWidow']
+DC = ['Superman', 'Batman', 'WonderWoman', 'Aquaman']
+
+print('Герои Marvel: {}'.format(Marvel))
+print('Герои DC: {}'.format(DC))
+
+for mr in Marvel[:]:
+    for dc in DC[:]:
+        if dc == mr:
+            Marvel.remove(dc)
+print('The Marvel only Heroes: {}'.format(Marvel))
